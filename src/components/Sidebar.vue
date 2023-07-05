59bea0f9d60e84ba927a597474caf5af4b5353db
Thang <cmthang2407@gmail.com>
2023-07-05 17:37:25 +0700

    update

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,50 @@
+<template>
+ <aside>
+    <div class="logo">
+        <img src="../assets/vue.svg" alt="Vue"/>
+    </div>
+
+    <div class="menu-toggle-wrap">
+        <button class="menu-toggle">
+            <span class="material-icons">
+
+            </span>
+        </button>
+    </div>
+ </aside>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+aside {
+    display: flex;
+    flex-direction: column;
+    width: calc(2rem + 32px);
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 1rem;
+
+    background-color: var(--dark);
+    color: var(--light);
+    transition: 0.2s ease-out;
+
+    .logo {
+        margin-bottom: 1rem;
+
+        img {
+            margin-bottom: 1rem;
+
+            img {
+                width: 2rem;
+            }
+        }
+    }
+
+    @media (max-width: 768px) {
+        position: fixed;
+        z-index: 99;
+    }
+}
+</style> 
